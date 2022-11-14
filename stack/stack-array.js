5b3d829a16387b870b87fe0f314018cbf0f8e486
aifuxi <aifuxi.js@gmail.com>
2022-11-14 23:10:57 +0800

    feat: stack array

---
+++ stack/stack-array.js
@@ -0,0 +1,47 @@
+/**
+ * 使用array创建stack
+ */
+class StackArray {
+  constructor() {
+    this.items = [];
+  }
+
+  // 添加一个（或几个）新元素到栈顶。
+  push(element) {
+    this.items.push(element);
+  }
+
+  // 移除栈顶的元素，同时返回被移除的元素。
+  pop() {
+    return this.items.pop();
+  }
+
+  // 返回栈顶的元素，不对栈做任何修改（该方法不会移除栈顶的元素，仅仅返回它）。
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  // 如果栈里没有任何元素就返回 true，否则返回 false。
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  // 移除栈里的所有元素。
+  clear() {
+    this.items = [];
+  }
+
+  // 返回栈里的元素个数。该方法和数组的 length 属性很类似。
+  size() {
+    return this.items.length;
+  }
+}
+
+const stack1 = new StackArray();
+console.log(stack1.isEmpty());
+stack1.push(199);
+stack1.push(20);
+console.log(stack1);
+console.log(stack1.isEmpty());
+console.log(stack1.size());
+console.log(stack1.pop());
